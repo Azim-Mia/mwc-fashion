c6fd2d80f6f5a7b5335e0e559b65a0d40f424284
Azim Mia <141178400+Azim-Mia@users.noreply.github.com>
2025-01-24 23:11:31 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+//import Image from "next/image";
 import Slider from '@/components/Slider';
 import ProductList  from '@/components/ProductList'
 import CategoryList from  '@/components/CategoryList'
